ec3032755c8f6046c2afc986281096644af5aa66
Omkar <nzomkar@gmail.com>
2021-01-14 00:40:08 +0000

    updated run script to accomodate csv_writer.cpp in compilation step

--- build/run.sh
+++ build/run.sh
@@ -2,5 +2,5 @@
 # run script for the cpp bsv parser
 # must provide path to blocks as object file arg
 
-g++ -std=c++17 ../main.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
+g++ -std=c++17 ../main.cpp ../csv_writer.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
 ./main.o /mnt/nvme2/data/blocks/
