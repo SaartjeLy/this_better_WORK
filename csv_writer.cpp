46a6315d57d4f68a02ca41025175b4f3954446ed
Omkar <nzomkar@gmail.com>
2021-01-22 02:36:11 +0000

    optimised line construction in write_block, added a mutex to write_line, added a existence check for filename in constructor for writing the header row or not

--- csv_writer.cpp
+++ csv_writer.cpp
@@ -1,9 +1,15 @@
 #include "csv_writer.h"
 #include <iostream>
+#include <filesystem>
 
 CSV_WRITER::CSV_WRITER(std::string filename) : filename(filename) {
-    csv_file.open(filename, std::ios_base::app);
-    write_line("file_name,magic_number,block_size,version,previous_block_hash,merkle_root,time,bits,nonce");
+    if(std::filesystem::exists("./"+filename)) {
+        csv_file.open(filename, std::ios_base::app);
+    }
+    else {
+        csv_file.open(filename, std::ios_base::app);
+        write_line("magic_number,block_size,version,previous_block_hash,merkle_root,time,bits,nonce");    
+    }
 }
 
 /**
@@ -12,7 +18,9 @@ CSV_WRITER::CSV_WRITER(std::string filename) : filename(filename) {
  * @param line the line of comma separated data to be written to the CSV
  */
 void CSV_WRITER::write_line(std::string line) {
+    mutex.lock();
     csv_file << line << "\n";
+    mutex.unlock();
 }
 
 /**
@@ -22,23 +30,16 @@ void CSV_WRITER::write_line(std::string line) {
  * @param block unordered map of block data
  */
 void CSV_WRITER::write_block(std::string block_number, std::unordered_map<std::string, std::any>* block) {
-    std::string line;
-    std::string magic_number;
-    std::string block_size;
-    std::string version;
-    std::string previous_block_hash;
-    std::string merkle_root;
-    std::string time;
-    std::string bits;
-    std::string nonce; 
+    std::string* line = new std::string;
 
     std::unordered_map<std::string, std::any>::iterator preamble_it = (*block).find("preamble");
     if (preamble_it != (*block).end()) {
         std::unordered_map<std::string, std::string> preamble = std::any_cast<std::unordered_map<std::string, std::string>>(preamble_it->second);
 
         // probably need more error checks here
-        magic_number = preamble.find("magic_number")->second;
-        block_size = preamble.find("block_size")->second;
+
+        *line += preamble.find("magic_number")->second + ","; 
+        *line += preamble.find("block_size")->second + ","; 
     }
     else {
         perror("Can't find preamble in block");
@@ -50,23 +51,23 @@ void CSV_WRITER::write_block(std::string block_number, std::unordered_map<std::s
         std::unordered_map<std::string, std::string> header = std::any_cast<std::unordered_map<std::string, std::string>>(header_it->second);
         
         // probably need more error checks here
-        version = header.find("version")->second;
-        previous_block_hash = header.find("previous_block_hash")->second;
-        merkle_root = header.find("merkle_root")->second;
-        time = header.find("time")->second;
-        bits = header.find("bits")->second;
-        nonce = header.find("nonce")->second;
+
+        *line += header.find("version")->second + ",";
+        *line += header.find("previous_block_hash")->second + ","; 
+        *line += header.find("merkle_root")->second + ","; 
+        *line += header.find("time")->second + ","; 
+        *line += header.find("bits")->second + ","; 
+        *line += header.find("nonce")->second;
+
     }
     else {
         perror("Can't find header in block");
         exit(1);
     }
 
-    // construct line to be written to the csv file
-    line = block_number + "," + magic_number + "," + block_size + "," + version + "," + previous_block_hash +\
-             "," + merkle_root + "," + time + "," + bits + "," + nonce;
-
-    write_line(line);
+    write_line(*line);
+    
+    delete line;
 }
 
 void CSV_WRITER::close() {
