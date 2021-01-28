48d99bcaf78a8bbc6079871b7a3e7dda94eac57b
Omkar <nzomkar@gmail.com>
2021-01-28 22:43:28 +0000

    changed compilation command in run script to reflect project directory restructure

--- build/run.sh
+++ build/run.sh
@@ -2,5 +2,5 @@
 # run script for the cpp bsv parser
 # must provide path to blocks as object file arg
 
-g++ -std=c++17 ../main.cpp ../csv_writer.cpp ../json_writer.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
+g++ -std=c++17 ../src/main.cpp ../src/csv_writer.cpp ../src/json_writer.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
 ./main.o /mnt/nvme2/data/blocks/
