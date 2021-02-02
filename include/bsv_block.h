3236f9e66d6650967899bb94ab1a1217a82a4f35
Omkar <nzomkar@gmail.com>
2021-02-02 22:40:37 +0000

    change string pointer field types to strings

--- include/bsv_block.h
+++ include/bsv_block.h
@@ -8,14 +8,14 @@ class BSV_BLOCK {
     private:
         uint32_t* ptr;
         std::string* file_data;
-        std::string* magic_number;
-        std::string* block_size;
-        std::string* version;
-        std::string* previous_block_hash;
-        std::string* merkle_root;
-        std::string* time;
-        std::string* bits;
-        std::string* nonce;
+        std::string magic_number;
+        std::string block_size;
+        std::string version;
+        std::string previous_block_hash;
+        std::string merkle_root;
+        std::string time;
+        std::string bits;
+        std::string nonce;
         unsigned long number_of_transactions;
         std::vector<std::string>* transactions;
 
@@ -30,22 +30,22 @@ class BSV_BLOCK {
         void set_number_of_transactions (unsigned long num_of_transactions);
         void add_transaction(std::string* transaction);
 
-        unsigned long hex_to_int(std::string* hex_string);
-        std::string* read_bytes(int bytes);
+        unsigned long hex_to_int(std::string hex_string);
+        std::string read_bytes(int bytes);
         unsigned long read_variable_bytes();
         void parse_block();
     public:
         BSV_BLOCK(uint32_t* ptr, std::string* data);
         ~BSV_BLOCK();
 
-        std::string* get_magic_number();
-        std::string* get_block_size();
-        std::string* get_version();
-        std::string* get_previous_block_hash();
-        std::string* get_merkle_root();
-        std::string* get_time();
-        std::string* get_bits();
-        std::string* get_nonce();
+        std::string get_magic_number();
+        std::string get_block_size();
+        std::string get_version();
+        std::string get_previous_block_hash();
+        std::string get_merkle_root();
+        std::string get_time();
+        std::string get_bits();
+        std::string get_nonce();
         unsigned long get_number_of_transactions();
         std::vector<std::string>* get_transactions();
 };
