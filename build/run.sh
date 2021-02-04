1fc080f6a515fa12505c07811ca9ed1282d4b8d6
Omkar <nzomkar@gmail.com>
2021-02-04 23:36:22 +0000

    modified compile command to includen new classes

--- build/run.sh
+++ build/run.sh
@@ -2,5 +2,5 @@
 # run script for the cpp bsv parser
 # must provide path to blocks as object file arg
 
-g++ -std=c++17 ../src/main.cpp ../src/csv_writer.cpp ../src/json_writer.cpp ../src/bsv_block.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
+g++ -std=c++17 ../src/main.cpp ../src/csv_writer.cpp ../src/json_writer.cpp ../src/bsv_block.cpp ../src/bsv_transaction.cpp ../src/bsv_input.cpp ../src/bsv_output.cpp -I /home/supermicro/Documents/omkar/taskflow -O2 -pthread -lstdc++fs -o main.o
 ./main.o /mnt/nvme2/data/blocks/
