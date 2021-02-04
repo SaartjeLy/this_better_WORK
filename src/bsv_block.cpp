63fa101f541b4319de392b95ea6e5145be403816
Omkar <nzomkar@gmail.com>
2021-02-04 03:03:54 +0000

    refactor parse_block function to reflect removal of getters & setters

--- src/bsv_block.cpp
+++ src/bsv_block.cpp
@@ -6,20 +6,19 @@ BSV_BLOCK::BSV_BLOCK(uint32_t* ptr, std::string* data) : file_data(data), ptr(pt
 
 void BSV_BLOCK::parse_block() {
     // preamble
-    set_magic_number();
-    set_block_size();
+    magic_number = read_bytes(4);
+    block_size = read_bytes(4);
 
     // header
-    set_version();
-    set_previous_block_hash();
-    set_merkle_root();
-    set_time();
-    set_bits();
-    set_nonce();
+    version = read_bytes(4);
+    previous_block_hash = read_bytes(32);
+    merkle_root = read_bytes(32);
+    time = read_bytes(4);
+    bits = read_bytes(4);
+    nonce = read_bytes(4);
 
     // transactions
-    unsigned long number_of_transactions = read_variable_bytes();
-    set_number_of_transactions(number_of_transactions);
+    number_of_transactions = read_variable_bytes();
 
     for (int i=0; i<number_of_transactions; i++) {
         std::string version = read_bytes(4);
@@ -94,19 +93,16 @@ unsigned long BSV_BLOCK::read_variable_bytes() {
     if (val == 253) {
         std::string two_byte_hex_string = read_bytes(2);
         unsigned long two_byte_val = hex_to_int(two_byte_hex_string);
-        // delete two_byte_hex_string;
         return two_byte_val;
     }
     else if (val == 254) {
         std::string four_byte_hex_string = read_bytes(4);
         unsigned long four_byte_val = hex_to_int(four_byte_hex_string);
-        // delete four_byte_hex_string;
         return four_byte_val;
     }
     else if (val == 255) {
         std::string eight_byte_hex_string = read_bytes(8);
         unsigned long eight_byte_val = hex_to_int(eight_byte_hex_string);
-        // delete eight_byte_hex_string;
         return eight_byte_val;
     }
     else if (val > 255) {
@@ -114,90 +110,7 @@ unsigned long BSV_BLOCK::read_variable_bytes() {
         exit(1);
     }
 
-    // delete hex_string;
-
     return val;
-}
-
-std::string BSV_BLOCK::get_magic_number() {
-    return magic_number;
-}
-
-std::string BSV_BLOCK::get_block_size() {
-    return block_size;
-}
-
-std::string BSV_BLOCK::get_version() {
-    return version;
-}
-
-std::string BSV_BLOCK::get_previous_block_hash() {
-    return previous_block_hash;
-}
-
-std::string BSV_BLOCK::get_merkle_root() {
-    return merkle_root;
-}
-
-std::string BSV_BLOCK::get_time() {
-    return time;
-}
-
-std::string BSV_BLOCK::get_bits() {
-    return bits;
-}
-
-std::string BSV_BLOCK::get_nonce() {
-    return nonce;
-}
-
-unsigned long BSV_BLOCK::get_number_of_transactions() {
-    return number_of_transactions;
-}
-
-std::vector<std::string>* BSV_BLOCK::get_transactions() {
-    return transactions;
-}
-
-void BSV_BLOCK::set_magic_number() {
-    magic_number = read_bytes(4);
-}
-
-void BSV_BLOCK::set_block_size() {
-    block_size = read_bytes(4);
-}
-
-void BSV_BLOCK::set_version() {
-    version = read_bytes(4);
-}
-
-void BSV_BLOCK::set_previous_block_hash() {
-    previous_block_hash = read_bytes(32);
-}
-
-void BSV_BLOCK::set_merkle_root() {
-    merkle_root = read_bytes(32);
-}
-
-void BSV_BLOCK::set_time() {
-    time = read_bytes(4);
-}
-
-void BSV_BLOCK::set_bits() {
-    bits = read_bytes(4);
-}
-
-void BSV_BLOCK::set_nonce() {
-    nonce = read_bytes(4);
-}
-
-void BSV_BLOCK::set_number_of_transactions(unsigned long data) {
-    number_of_transactions = data;
-}
-
-void BSV_BLOCK::add_transaction(std::string* transaction) {
-    //TODO: implement
-    ;
 }
 
 BSV_BLOCK::~BSV_BLOCK() {
