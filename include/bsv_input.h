02aab0e059970d772d2354f0e08156b46b936167
Omkar <nzomkar@gmail.com>
2021-02-04 23:37:03 +0000

    added BSV_INPUT class declaration

---
+++ include/bsv_input.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+#ifndef BSV_INPUT_H
+#define BSV_INPUT_H
+
+class BSV_INPUT {
+    private:
+    public:
+        BSV_INPUT();
+        ~BSV_INPUT();
+
+        std::string pre_transaction_hash;
+        std::string pre_transaction_out_index;
+        unsigned long script_length;
+        std::string script;
+        std::string sequence;
+};
+
+#endif
