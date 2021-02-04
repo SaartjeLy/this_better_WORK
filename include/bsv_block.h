aeb26cdb2ab9c4edb5826281849d35e9b53b691b
Omkar <nzomkar@gmail.com>
2021-02-04 03:03:22 +0000

    removed getters & setters, made data field public

--- include/bsv_block.h
+++ include/bsv_block.h
@@ -8,6 +8,16 @@ class BSV_BLOCK {
     private:
         uint32_t* ptr;
         std::string* file_data;
+
+        void parse_block();
+    public:
+        BSV_BLOCK(uint32_t* ptr, std::string* data);
+        ~BSV_BLOCK();
+
+        unsigned long hex_to_int(std::string hex_string);
+        std::string read_bytes(int bytes);
+        unsigned long read_variable_bytes();
+
         std::string magic_number;
         std::string block_size;
         std::string version;
@@ -17,37 +27,6 @@ class BSV_BLOCK {
         std::string bits;
         std::string nonce;
         unsigned long number_of_transactions;
-        std::vector<std::string>* transactions;
-
-        void set_magic_number();
-        void set_block_size();
-        void set_version();
-        void set_previous_block_hash();
-        void set_merkle_root();
-        void set_time();
-        void set_bits();
-        void set_nonce();
-        void set_number_of_transactions (unsigned long num_of_transactions);
-        void add_transaction(std::string* transaction);
-
-        unsigned long hex_to_int(std::string hex_string);
-        std::string read_bytes(int bytes);
-        unsigned long read_variable_bytes();
-        void parse_block();
-    public:
-        BSV_BLOCK(uint32_t* ptr, std::string* data);
-        ~BSV_BLOCK();
-
-        std::string get_magic_number();
-        std::string get_block_size();
-        std::string get_version();
-        std::string get_previous_block_hash();
-        std::string get_merkle_root();
-        std::string get_time();
-        std::string get_bits();
-        std::string get_nonce();
-        unsigned long get_number_of_transactions();
-        std::vector<std::string>* get_transactions();
 };
 
 #endif
