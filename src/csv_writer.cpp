d9e17a5099b358093ba09744e6f9d381fca6254a
Omkar <nzomkar@gmail.com>
2021-02-09 05:03:27 +0000

    modified write_block function to construct a line to be written to the csv file using new BSV_BLOCK object after BSV_BLOCK refactor, modified constructor to include "block number" as a header field

--- src/csv_writer.cpp
+++ src/csv_writer.cpp
@@ -8,7 +8,7 @@ CSV_WRITER::CSV_WRITER(std::string filename) : filename(filename) {
     }
     else {
         csv_file.open(filename, std::ios_base::app);
-        write_line("magic_number,block_size,version,previous_block_hash,merkle_root,time,bits,nonce");    
+        write_line("block_number,magic_number,block_size,version,previous_block_hash,merkle_root,time,bits,nonce");    
     }
 }
 
@@ -29,45 +29,19 @@ void CSV_WRITER::write_line(std::string line) {
  * @param block_number the block number
  * @param block unordered map of block data
  */
-void CSV_WRITER::write_block(std::string block_number, std::unordered_map<std::string, std::any>* block) {
-    std::string* line = new std::string;
+void CSV_WRITER::write_block(uint32_t block_number, BSV_BLOCK block) {
+    std::string line;
 
-    std::unordered_map<std::string, std::any>::iterator preamble_it = (*block).find("preamble");
-    if (preamble_it != (*block).end()) {
-        std::unordered_map<std::string, std::string> preamble = std::any_cast<std::unordered_map<std::string, std::string>>(preamble_it->second);
+    line += std::to_string(block_number) + ",";
+    line += block.magic_number + ",";
+    line += block.block_size + ",";
+    line += block.version + ",";
+    line += block.previous_block_hash + ",";
+    line += block.merkle_root + ",";
+    line += block.time + ",";
+    line += block.nonce + ",";
 
-        // probably need more error checks here
-
-        *line += preamble.find("magic_number")->second + ","; 
-        *line += preamble.find("block_size")->second + ","; 
-    }
-    else {
-        perror("Can't find preamble in block");
-        exit(1);
-    }
-
-    std::unordered_map<std::string, std::any>::iterator header_it = (*block).find("header");
-    if (header_it != (*block).end()) {
-        std::unordered_map<std::string, std::string> header = std::any_cast<std::unordered_map<std::string, std::string>>(header_it->second);
-        
-        // probably need more error checks here
-
-        *line += header.find("version")->second + ",";
-        *line += header.find("previous_block_hash")->second + ","; 
-        *line += header.find("merkle_root")->second + ","; 
-        *line += header.find("time")->second + ","; 
-        *line += header.find("bits")->second + ","; 
-        *line += header.find("nonce")->second;
-
-    }
-    else {
-        perror("Can't find header in block");
-        exit(1);
-    }
-
-    write_line(*line);
-    
-    delete line;
+    write_line(line);
 }
 
 void CSV_WRITER::close() {
